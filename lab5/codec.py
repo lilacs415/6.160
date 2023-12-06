8a62b18a15076937a2d432b949bd96f717427c43
Lila Chen <lilacs@gmail.com>
2023-12-06 03:10:32 -0500

    all pass

--- lab5/codec.py
+++ lab5/codec.py
@@ -20,9 +20,8 @@ alphanumeric = (string.ascii_letters + string.digits).encode('ascii')
 #   that chopped part should produce the corresponding part of the original string,
 #   modulo things that might have gotten cut off at each end.
 
-FROM_HEX = {key: val for key, val in zip(printable[65:].decode(), [i for i in range(16)])}
-TO_HEX = {val: key for key, val in FROM_HEX.items()}
-OTHER_HEX = {key: val for key, val in zip(printable[65:].decode(), ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'])}
+TO_HEX = {key: val for val, key in zip(printable[65:].decode(), [i for i in range(16)])}
+OTHER_HEX = {key: val for key, val in zip(printable[65:].decode(), [_ for _ in '0123456789abcdef'])}
 
 def encode(input):
     data = bytearray(input)
@@ -31,9 +30,6 @@ def encode(input):
         if byte in alphanumeric:
             buffer.append(byte)
         else:
-            # print(byte % 16)
-            # print(TO_HEX[byte // 16] + TO_HEX[byte % 16])
-            # print((TO_HEX[byte // 16] + TO_HEX[byte % 16]).encode('ascii'))
             buffer.extend(b"!" + (TO_HEX[byte // 16] + TO_HEX[byte % 16]).encode('ascii'))
     return bytes(buffer)
 
@@ -49,22 +45,19 @@ def decode(buf):
     i = min(an, np)
     
     while i < len(buf):
-        if buf[i] == ord("!") and i + 2 < len(buf):
-            hex_str = buf[i+1:i+3]
-            # print('hex_str', hex_str)
-            if len(hex_str) == 2:
-                # print(hex_str[0], hex_str[1])
-                hex_byte = OTHER_HEX[chr(hex_str[0])] + OTHER_HEX[chr(hex_str[1])]
-                decoded_byte = bytes.fromhex(hex_byte)
-                # print(decoded_byte)
-                # print(bytes(decoded_byte))
-                # print(decoded_byte.encode('ascii'))
-                output.extend(decoded_byte)
-            i += 3
+        if buf[i] == ord("!"):
+            if i + 2 < len(buf):
+                hex_str = buf[i+1:i+3]
+                if len(hex_str) == 2:
+                    hex_byte = OTHER_HEX[chr(hex_str[0])] + OTHER_HEX[chr(hex_str[1])]
+                    decoded_byte = bytes.fromhex(hex_byte)
+                    output.extend(decoded_byte)
+                i += 3
+            else:
+                break
         else:
             output.append(buf[i])
             i += 1
-
     return bytes(output)
 
 def encode_and_decode(i):
@@ -72,13 +65,3 @@ def encode_and_decode(i):
     dec = decode(enc)
     print(len(i), len(enc), len(dec))
     print(i, "->", enc, "->", dec)
-
-# print(encode(b"\n"))
-# buffer = b'!0a'
-# buf = buffer[0:3-1]
-print(decode(b'!$'))
-# encode_and_decode(b"hello world")
-# encode_and_decode(b"\x00\x01\x02\x03")
-# print(bytes.fromhex('0a'))
-# encode_and_decode(b"\n")
-# encode_and_decode(b"\n!\n\n")
--- lab5/test_codec.py
+++ lab5/test_codec.py
@@ -8,40 +8,35 @@ with atheris.instrument_imports():
 def test_str(data):
     fuzzer = atheris.FuzzedDataProvider(data)
     out = fuzzer.ConsumeBytes(32)
-    # print("START", out)
+
     encoding = codec.encode(out)
 
     assert type(encoding) == bytes, "encoding must be bytes"
-    # print(encoding)
-    # print([char in codec.printable for char in encoding])
     assert all([char in codec.printable for char in encoding]), "encoding must be printable"
     assert codec.encode(b"Apples") == b"Apples", "encoding must be 1 to 1"
     assert len(encoding) <= 3 * len(out), "too long"
 
     dec = codec.decode(encoding)
+
     assert type(dec) == bytes, "decoded must be bytes"
-    # print(out)
     assert out == dec, 'wrong'
 
 def test_recoverable(data):
     fuzzer = atheris.FuzzedDataProvider(data)
     out = fuzzer.ConsumeBytes(32)
-    # print("START", out)
+
     encoding = codec.encode(out)
-    # print(encoding)
 
     cut_begin = random.randint(0, len(encoding) // 2)
     cut_end = random.randint(0, len(encoding) // 2)
-    # print(cut_begin, cut_end)
 
     encoding = encoding[cut_begin:len(encoding) - cut_end]
-
     dec = codec.decode(encoding)
-    # print(dec, out)
+
     assert dec in out, "not recoverable"
 
 
 if __name__ == "__main__":
-    test = test_str
+    test = test_recoverable
     atheris.Setup(sys.argv, test)
     atheris.Fuzz()
